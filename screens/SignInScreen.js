dd7574b3712cd0ef3fbebf3310e3244cee99748e
ChoiHyungKeun <gudrms1592@naver.com>
2022-08-21 00:57:04 +0900

    화면 셋팅

---
+++ screens/SignInScreen.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import {
+  Alert,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import BorderedInput from '../components/BorderedInput';
+import CustomButton from '../components/CustomButton';
+import SignButtons from '../components/SignButtons';
+import SignForm from '../components/SignInForm';
+
+function SignInScreen({navigation, route}) {
+  const {isSignUp} = route.params ?? {};
+
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const createChangeTextHandler = name => value => {
+    setForm({...form, [name]: value});
+  };
+
+  const onSubmit = () => {
+    Keyboard.dismiss();
+    console.log(form);
+  };
+  return (
+    <KeyboardAvoidingView
+      style={styles.keyboardAvoidingView}
+      behavior={Platform.select({ios: 'padding'})}>
+      <SafeAreaView style={styles.fullscreen}>
+        <Text style={styles.text}>PublicGallery</Text>
+        <View style={styles.form}>
+          <SignForm
+            isSignUp={isSignUp}
+            onSubmit={onSubmit}
+            form={form}
+            createChangeTextHandler={createChangeTextHandler}
+          />
+          <SignButtons isSignUp={isSignUp} onSubmit={onSubmit} />
+        </View>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  keyboardAvoidingView: {
+    flex: 1,
+  },
+  fullscreen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  form: {
+    marginTop: 64,
+    width: '100%',
+    paddingHorizontal: 16,
+  },
+  buttons: {
+    marginTop: 64,
+  },
+});
+
+export default SignInScreen;
