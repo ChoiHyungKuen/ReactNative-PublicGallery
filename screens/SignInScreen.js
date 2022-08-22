16cd39fe6dec8e4a29bfadd6526323870dae5f38
ChoiHyungKeun <gudrms1592@naver.com>
2022-08-22 23:55:22 +0900

    사진업로드까지 완료

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -13,10 +13,15 @@ import BorderedInput from '../components/BorderedInput';
 import CustomButton from '../components/CustomButton';
 import SignButtons from '../components/SignButtons';
 import SignForm from '../components/SignInForm';
+import {signIn, signUp} from '../lib/auth';
+import {getUser} from '../lib/users';
+import {useUserContext} from '../context/UserContext';
 
 function SignInScreen({navigation, route}) {
   const {isSignUp} = route.params ?? {};
+  const {setUser} = useUserContext();
 
+  const [loading, setLoading] = useState();
   const [form, setForm] = useState({
     email: '',
     password: '',
@@ -27,10 +32,44 @@ function SignInScreen({navigation, route}) {
     setForm({...form, [name]: value});
   };
 
-  const onSubmit = () => {
+  const onSubmit = async () => {
     Keyboard.dismiss();
-    console.log(form);
+
+    const {email, password, confirmPassword} = form;
+
+    if (isSignUp && password !== confirmPassword) {
+      Alert.alert('실패', '비밀번호가 일치하지 않습니다.');
+      console.log({password, confirmPassword});
+      return;
+    }
+
+    setLoading(true);
+    const info = {email, password};
+
+    try {
+      const {user} = isSignUp ? await signUp(info) : await signIn(info);
+      const profile = await getUser(user.uid);
+      if (!profile) {
+        navigation.navigate('Welcome', {uid: user.uid});
+      } else {
+        setUser(user);
+      }
+      console.log(user);
+    } catch (e) {
+      const messages = {
+        'auth/email-already-in-use': '이미 가입된 이메일입니다.',
+        'auth/wrong-password': '잘못된 비밀번호입니다.',
+        'auth/user-not-found': '존재하지 않는 계정입니다.',
+        'auth/invalid-email': '유효하지 않은 이메일 주소입니다.',
+      };
+
+      const msg = messages[e.code] || `${isSignUp ? '가입' : '로그인'} 실패`;
+      Alert.alert('실패', msg);
+    } finally {
+      setLoading(false);
+    }
   };
+
   return (
     <KeyboardAvoidingView
       style={styles.keyboardAvoidingView}
@@ -44,7 +83,11 @@ function SignInScreen({navigation, route}) {
             form={form}
             createChangeTextHandler={createChangeTextHandler}
           />
-          <SignButtons isSignUp={isSignUp} onSubmit={onSubmit} />
+          <SignButtons
+            isSignUp={isSignUp}
+            onSubmit={onSubmit}
+            loading={loading}
+          />
         </View>
       </SafeAreaView>
     </KeyboardAvoidingView>
